93ce83c4d2248617ea84bc27f2d29d22230debf5
Alex45452 <48757422+Alex45452@users.noreply.github.com>
2019-08-11 13:37:25 +0300

    Add files via upload

--- fwed.py
+++ fwed.py
@@ -1,7 +1,7 @@
 def count(x):
     operation = x*10/3+100
     return operation
-def count_list(numbers):
+def count_list(*numbers):
     for number in numbers:
         print(count(number))
-numbers = map()
+count_list(3, 6, 9)
