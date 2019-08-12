e89e6d00b150b748f3ef5a0391015c96be43fbc7
Alex45452 <48757422+Alex45452@users.noreply.github.com>
2019-08-12 15:28:08 +0300

    Update fwed.py

--- fwed.py
+++ fwed.py
@@ -1,7 +1,9 @@
 def count(x):
     operation = x*10/3+100
     return operation
-def count_list(my_list):
+def count_list(my_list):    
+    result = []    
     for number in my_list:
-        print(count(number))
-count_list(3, 6, 9])
+        result.append(count(number))
+    return result    
+print(count_list([3, 6, 9]))
